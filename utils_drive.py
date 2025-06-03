bc74f02e324bf9211d44f45699eb03576a6511c9
thuycq <thuycq@uel.edu.vn>
2025-06-03 15:57:19 +0700

    Update utils_drive.py

--- utils_drive.py
+++ utils_drive.py
@@ -1,22 +1,33 @@
+import os
+import json
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 
-# Đường dẫn file JSON service account
-SERVICE_ACCOUNT_FILE = 'syllabus-app-drive-api.json'
-
-# ID của folder Drive đích
+# ID của folder Drive
 FOLDER_ID = '1vtziPO7_zj7-JJlnxOqP568NV_nP1sK7'
 
-# Tạo service kết nối Drive API
 def create_drive_service():
     SCOPES = ['https://www.googleapis.com/auth/drive']
-    creds = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+    # Kiểm tra có biến môi trường không
+    json_content = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
+    if json_content:
+        # Nếu chạy trên Streamlit Cloud
+        info = json.loads(json_content)
+        creds = service_account.Credentials.from_service_account_info(
+            info, scopes=SCOPES
+        )
+    else:
+        # Nếu chạy LOCAL → dùng file
+        LOCAL_JSON_PATH = 'syllabus-app-drive-api.json'
+        creds = service_account.Credentials.from_service_account_file(
+            LOCAL_JSON_PATH, scopes=SCOPES
+        )
+
     service = build('drive', 'v3', credentials=creds)
     return service
 
-# Upload file lên Drive
 def upload_file_to_drive(filename, filepath, mimetype='application/vnd.openxmlformats-officedocument.wordprocessingml.document'):
     service = create_drive_service()
 
